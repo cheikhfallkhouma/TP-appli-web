7af8de0a3a9618bdfc4abffa1c5b627086ae6dac
cheikhfallkhouma <56485192+cheikhfallkhouma@users.noreply.github.com>
2020-11-25 01:13:50 +0100

    Add files via upload

---
+++ tp4/js/form-jquery-validation.js
@@ -0,0 +1,40 @@
+window.onload = function () {
+  document.querySelector("#name").addEventListener("keyup", function (event) {
+    document.getElementById("nameCounter").textContent = `${event.target.value.length} car.`;
+   });
+
+ document.querySelector("#firstname").addEventListener("keyup", function (event) {
+  document.getElementById("nameCounter").textContent = `${event.target.value.length} car.`;});
+  
+
+  document.querySelector("#gps").addEventListener("click", function (event) {
+    event.preventDefault();
+    getLocation();
+  });
+
+  document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    contactStore.add(
+      document.querySelector("#name").value,
+      document.querySelector("#firstname").value,
+      document.querySelector("#birth").value,
+      document.querySelector("#adresse").value,
+      document.querySelector("#mail").value
+    );
+
+    const contactList = contactStore.getList();
+
+    
+
+    for (var contact of contactList) {
+      document.querySelector("table tbody").innerHTML  += `
+        <tr>
+            <td> ${contact.name}</td>
+            <td> ${contact.firstname}</td>
+            <td> ${contact.date}</td>
+            <td> <a href="https://maps.google.com/?q=${contact.adress}">${contact.adress}</a></td>
+            <td> <a href="mailto:${contact.mail}">${contact.mail}</a></td>
+        <tr>`;
+    }
+  });
+ };
